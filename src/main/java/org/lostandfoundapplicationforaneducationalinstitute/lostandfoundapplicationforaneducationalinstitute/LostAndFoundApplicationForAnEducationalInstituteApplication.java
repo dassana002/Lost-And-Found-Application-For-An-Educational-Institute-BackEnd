96202ef62cbcd9a353f0396cfd0fb6dd74ed19b3
Dassana <dassana002@gmail.com>
2025-05-09 10:32:04 +0530

    first commit

---
+++ src/main/java/org/lostandfoundapplicationforaneducationalinstitute/lostandfoundapplicationforaneducationalinstitute/LostAndFoundApplicationForAnEducationalInstituteApplication.java
@@ -0,0 +1,13 @@
+package org.lostandfoundapplicationforaneducationalinstitute.lostandfoundapplicationforaneducationalinstitute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundApplicationForAnEducationalInstituteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostAndFoundApplicationForAnEducationalInstituteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lostandfoundapplicationforaneducationalinstitute/lostandfoundapplicationforaneducationalinstitute/LostAndFoundApplicationForAnEducationalInstituteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lostandfoundapplicationforaneducationalinstitute.lostandfoundapplicationforaneducationalinstitute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAndFoundApplicationForAnEducationalInstituteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
