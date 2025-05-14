a13ff8827d5d5bfa25684169624bb693a92770df
Dassana <dassana002@gmail.com>
2025-05-14 10:29:29 +0530

    Request Controller create and Testing

--- src/main/java/org/lostandfoundapplicationforaneducationalinstitute/lostandfoundapplicationforaneducationalinstitute/controller/ItemController.java
+++ src/main/java/org/lostandfoundapplicationforaneducationalinstitute/lostandfoundapplicationforaneducationalinstitute/controller/ItemController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/Item")
+@RequestMapping("/api/v1/Items")
 @RequiredArgsConstructor
 public class ItemController {
 
